b6f626e5558a581ca6febec714f8f8f51e518db9
Alvaro Gomez <alvarogope99@gmail.com>
2026-03-24 11:41:48 +0000

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from clerk_backend_api import Clerk
-from .routes import challenge, webhooks
+from .routes import challenge, webhook
 import os
 
 clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
@@ -10,9 +10,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
     allow_credentials=True,
     allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(challenge.router, prefix="/api")
